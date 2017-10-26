29e655b2650dfd9cf80a8a6b758fbed64ef420e1
d0iasm <doiasami1219@gmail.com>
2017-10-26 23:52:49 +0900

    Update using initializer list

--- basic.cpp
+++ basic.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
 int main() {
-  int a, b;
+  int a=5;
+  int b(3);
+  int c{2};
   int result;
 
-  a = 5;
-  b = 2;
-  a = a + 1;
-  result = a - b;
+  a = a + b;
+  result = a - c;
   
   std::cout << result;
   return 0;
