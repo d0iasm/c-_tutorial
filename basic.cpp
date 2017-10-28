089ad6a044c7f73fea847b5812750426d972c830
d0iasm <doiasami1219@gmail.com>
2017-10-28 20:52:15 +0900

    Add include string

--- basic.cpp
+++ basic.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
+#include <string>
 
 int main() {
   int a=5;
   int b(3);
   int c{2};
   int result;
+  std::string mystring;
 
   a = a + b;
   result = a - c;
+
+  mystring = "This is a string";
   
-  std::cout << result;
+  std::cout << mystring << std::endl;
   return 0;
 }
