59f7c71307cc9a56e6980f53d9a4cc510e4e716c
d0iasm <doiasami1219@gmail.com>
2017-10-30 23:57:20 +0900

    Update define that is preprocessor directives instead of const

--- basic.cpp
+++ basic.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
-const double pi = 3.1415926;
-const char newline = '\t';
+#define PI 3.1415926
+#define NEWLINE '\t'
 
 int main() {
   double r = 5.0;
   double circle;
 
-  circle = 2 * pi * r;
+  circle = 2 * PI * r;
   std::cout << circle;
-  std::cout << newline;
+  std::cout << NEWLINE;
   return 0;
 }
