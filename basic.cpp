21ebc734ba8b0b9100f0ab958ae2eab5f4192f99
d0iasm <doiasami1219@gmail.com>
2017-11-02 22:29:48 +0900

    Update multiple variables of input and output

--- basic.cpp
+++ basic.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 
 #define PI 3.1415926
-#define NEWLINE '\t'
+#define NEWLINE '\n'
 
 int main() {
-  int r;
-  std::cin >> r;
-  
+  int r, r2;
+  std::cin >> r >> r2;
+  std::clog << "Inputted first radius is: " << r << NEWLINE;
+  std::clog << "Inputted second radius is: " << r2 << NEWLINE;
+
   double circle;
 
   circle = 2 * PI * r;
-  std::cout << circle;
-  std::cout << NEWLINE;
+  std::cout << circle << std::endl;
   return 0;
 }
