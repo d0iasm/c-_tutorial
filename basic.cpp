501c1d2390ae67682c1941c70515e45079cc3b6d
d0iasm <doiasami1219@gmail.com>
2017-11-03 21:10:58 +0900

    Add a file of structures

--- basic.cpp
+++ basic.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 
 #define PI 3.1415926
 #define NEWLINE '\n'
@@ -13,5 +15,21 @@ int main() {
 
   circle = 2 * PI * r;
   std::cout << circle << std::endl;
+
+  std::string mystr;
+  float price = 0;
+  int quantity = 0;
+  std::cout << "Enter price: ";
+  std::getline(std::cin, mystr);
+  std::stringstream(mystr) >> price;
+
+  std::cout << "Enter quantity: ";
+  std::getline(std::cin, mystr);
+  std::stringstream(mystr) >> quantity;
+
+  std::cout << price << " " << quantity << std::endl;
+
+  std::cout << "Total price: " << price * quantity << std::endl;
+
   return 0;
 }
