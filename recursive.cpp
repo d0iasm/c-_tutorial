ef40787ac202e8e0ead81faf3a64baa851ddd16b
d0iasm <doiasami1219@gmail.com>
2017-11-06 21:12:01 +0900

    Add a file of recursive

---
+++ recursive.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+long factorial (long a) {
+  if (a > 1)
+    return (a * factorial(a-1));
+  else
+    return 1;
+}
+
+int main () {
+  long number = 9;
+  std::cout << number << "! = " << factorial(number) << std::endl;
+  return 0;
+}
