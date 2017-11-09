87ee12c809732f74170b7dfe29ad3b53be5e70de
d0iasm <doiasami1219@gmail.com>
2017-11-09 15:49:17 +0900

    Add a function of dynamic meomry

--- datatypes.cpp
+++ datatypes.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <new>
 
 int foo [] = {16, 2, 77, 40, 12071};
 int n, result = 0;
@@ -68,6 +69,27 @@ int operation (int x, int y, int (*functocall)(int, int)) {
   return (g);
 }
 
+void dynamic_memory () {
+  int i, n;
+  int * p;
+  std::cout << "How many numbers would you like to type? ";
+  std::cin >> i;
+  p = new (std::nothrow) int[i];
+  if (p == nullptr) {
+    std::cout << "Error: memory could not be allocated";
+  } else {
+    for (n=0; n<i; n++) {
+      std::cout << "Enter number: ";
+      std::cin >> p[n];
+    }
+    std::cout << "You have entered: ";
+    for (n=0; n<i; n++) {
+      std::cout << p[n] << ",  ";
+    }
+    delete[] p;
+  }
+}
+
 int main () {
   for (n=0; n<5; ++n) {
     result += foo[n];
@@ -87,6 +109,8 @@ int main () {
   m = operation(7, 5, addtion);
   n = operation(20, m, minus);
   std::cout << n << std::endl;
+
+  dynamic_memory();
   
   return 0;
 }
