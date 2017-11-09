6ed5dac7c4b80aed57dd8712acbdff37322cb9ce
d0iasm <doiasami1219@gmail.com>
2017-11-09 16:23:02 +0900

    Update a class by using constructor

--- classes.cpp
+++ classes.cpp
@@ -3,21 +3,29 @@
 class Rectangle {
   int width, height;
 public:
-  void set_val (int, int);
+  Rectangle();
+  Rectangle(int, int);
   int area() {
     return width * height;
   } 
 };
 
-void Rectangle::set_val (int x, int y) {
+Rectangle::Rectangle () {
+  width = 5;
+  height = 5;
+}
+
+Rectangle::Rectangle (int x, int y) {
   width = x;
   height = y;
 }
 
 int main () {
-  Rectangle rect;
-  rect.set_val(3, 4);
+  Rectangle rect(3, 4);
+  Rectangle * rectb;
+  rectb = &rect;
   std::cout << "area: " << rect.area() << std::endl;
+  std::cout << "area: " << rectb->area() << std::endl;
 
   return 0;
 }
